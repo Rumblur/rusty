faa87ff1417bca5e72ae9f69afaa41c279449ec9
Zakhar Timoshenko <vp1984tanki@gmail.com>
2021-07-28 17:39:19 +0300

    Improve RCON output

--- cogs/rcon.py
+++ cogs/rcon.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 
 import discord
 from discord.ext import commands
@@ -16,7 +17,7 @@ class RCON(commands.Cog):
     async def rcon(self, ctx, ip=None):
         if ip:
             self.bot.rcon_cache[ctx.author.id] = {"ip": ip, "expires": datetime.datetime.now() + datetime.timedelta(
-                                                      minutes=10)}
+                minutes=10)}
             await ctx.send("Учетные данные кэшированы!")
         else:
             await ctx.send('Использование: `,login "ip"`')
@@ -38,8 +39,9 @@ class RCON(commands.Cog):
         try:
             with MCRcon(creds["ip"], secret.rcon_password, secret.rcon_port) as mcr:
                 response = mcr.command(command)
+                pretty_response = re.sub('(§[0-9a-fA-Fkmorln])|(§\[#[0-9a-fA-F]{1,6}])|(§$)', '', response)  # BadCoder
                 if len(response) <= 1800:
-                    await ctx.send("Ответ: ```{}```".format(response))
+                    await ctx.send("Ответ: ```{}```".format(pretty_response))
                 else:
                     await ctx.send("Ответ был слишком долгим.",
                                    file=discord.File(BytesIO(response.encode()), "response.txt"))
